d2e0387f5aad65689b5c8d413b3950ebea6966ba
yycx0328 <lyh_oracle@hotmail.com>
2020-04-17 17:11:23 +0800

    update

--- Models/Opcomunity/WechatPayConfig.cs
+++ Models/Opcomunity/WechatPayConfig.cs
@@ -9,13 +9,13 @@ namespace Models.Opcomunity
 {
     public class WechatPayConfig
     {
-        public static string APPID = "wxdb246e06e98f87e4";
-        public static string MchID = "1497669662"; 
+        public static string APPID = "xxx";
+        public static string MchID = "xxx"; 
         public static string UnifiedUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         public static string NotifyUrl = ConfigurationManager.AppSettings["WechatCallbackUrl"];
         public static string Body = "圈币";
         public static string TradeType = "APP";
-        public static string ApiKey = "87c6fc4251974716b0b94f1fdc349d8b"; 
+        public static string ApiKey = "xxx"; 
         public static string SignType = "MD5";
     }
 }
